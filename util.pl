ad8368005b5d455da1f01fc11e8dde339d6c9224
daniel <daniel@topoffice.(none)>
2013-03-14 22:16:25 +0100

    commented util.pl

--- util.pl
+++ util.pl
@@ -1,11 +1,14 @@
-list_empty([]).
-
+% print_list(+List)
+% recursively prints elements of list
 print_list([]) :- print('.').
 print_list([H|T]) :-
 	print(H),
+	(T = [] ; print(', ')),
 	print_list(T).
 
-% add width parameter, which should be M*N
+% print_sudoku1(+ValuesList)
+% TODO add width parameter, which should be M*N
+% BUG prints the sudoku rotated counter-clockwise 90 degrees
 print_sudoku1([]).
 print_sudoku1([H|Values]) :-
 	H = [X, Y, V],
@@ -13,11 +16,14 @@ print_sudoku1([H|Values]) :-
 	(Y = 3, format('~n', _) ; true),
 	print_sudoku1(Values).
 
+% print_sudoku(+Sudoku)
+% preprocessing for print_sudoku1
 print_sudoku(Sudoku) :-
 	Sudoku = sudoku(_, _, Values),
 	sort(Values, Sorted),
 	print_sudoku1(Sorted).
 
+% subtract - copied from swi-pl 'listing(subtract)'
 subtract([], _, []) :- !.
 subtract([A|C], B, D) :-
 	memberchk(A, B), !,
