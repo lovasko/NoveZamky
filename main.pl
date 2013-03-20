94a16a5f17544fd7f21a50e094301ccffea0413d
root <root@red.cz>
2013-03-20 06:29:43 +0100

    added time measuring

--- main.pl
+++ main.pl
@@ -5,6 +5,8 @@
 :- include('solve.pl').
 :- initialization(main_false).
 
+:- dynamic(start_time/1).
+
 % main(-SolvedSudoku)
 main(SolvedSudoku) :-
 	getopt(File, Method),
@@ -13,10 +15,19 @@ main(SolvedSudoku) :-
 	parse_sudoku(Sudoku),
 	format('Solving sudoku:~n', _),
 	print_sudoku(Sudoku),
+	user_time(Start),
+	asserta(start_time(Start)),
 	listify(Method, Sudoku, List), !,
 	solve(Sudoku, List, SolvedSudoku, 'nodebug'),
 	format('~nPossible solution: ~n', _),
 	print_sudoku(SolvedSudoku).
 
-main_false :- main(_), false.
-main_false :- format('Done.~n', _).
+main_false :- 
+	main(_), 
+	false.
+	
+main_false :- 
+	start_time(Start), 
+	user_time(End), 
+	Duration is End-Start,
+	format('Time: %dms~n', [Duration]).
