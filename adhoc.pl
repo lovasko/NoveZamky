24cf44bf39b6b11c7d99f5fc3be56522ce662812
root <root@red.cz>
2013-03-23 22:25:24 +0100

    optimized version of count_correct

--- adhoc.pl
+++ adhoc.pl
@@ -1,8 +1,27 @@
 % count_correct(+Sudoku, +Field, -Count)
 % outputs Count of correct posibilities for Field in scope of Sudoku
+count_correct1(_, _, _, 0, []).
+
+count_correct1(RowValues, ColumnValues, AreaValues, MaxPossible, [MaxPossible|Out]) :-
+	\+ member(MaxPossible, RowValues),
+	\+ member(MaxPossible, ColumnValues),
+	\+ member(MaxPossible, AreaValues),
+	Max2 is MaxPossible - 1,
+	count_correct1(RowValues, ColumnValues, AreaValues, Max2, Out), !.
+
+count_correct1(RowValues, ColumnValues, AreaValues, MaxPossible, Out) :-
+	Max2 is MaxPossible - 1,
+	count_correct1(RowValues, ColumnValues, AreaValues, Max2, Out), !.
+
 count_correct(Sudoku, Field, Count-Field) :-
-	bagof(CorrectField, correct(Sudoku, Field, CorrectField), Corrects),
-	length(Corrects, Count).
+	Field = [X, Y, Value],
+	get_row_values(Sudoku, [X, Y, Value], RowValues),
+	get_column_values(Sudoku, [X, Y, Value], ColumnValues),
+	get_area_values(Sudoku, [X, Y, Value], AreaValues),
+	Sudoku = sudoku(M, N, _),
+	MaxPossible is M * N,
+	count_correct1(RowValues, ColumnValues, AreaValues, MaxPossible, Out), !,
+	length(Out, Count).
 
 % not_done_yet(+In, -Out)
 % filters yet not set fields in In to Out
@@ -23,7 +42,7 @@ adhoc(sudoku(M, N, Fields), sudoku(M, N, Fields)) :-
 	NotDone = [].
 adhoc(sudoku(M, N, Fields), Solved) :-
 	not_done_yet(Fields, NotDone),
-	maplist(count_correct(sudoku(M, N, Fields)), NotDone, Rated),
+	maplist(cc(sudoku(M, N, Fields)), NotDone, Rated),
 	keysort(Rated, SortedRated),
 	[Rate-Field|_] = SortedRated,
 	correct(sudoku(M, N, Fields), Field, CorrectField),
