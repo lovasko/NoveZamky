38770c925fe0d3331119a1c16b64080accbadd45
root <root@red.cz>
2013-03-23 11:43:46 +0100

    fixed adhoc solving problems

--- adhoc.pl
+++ adhoc.pl
@@ -8,16 +8,19 @@ not_done_yet([], []).
 not_done_yet([[X, Y, 0]|In],[[X, Y, 0]|Out]) :- not_done_yet(In, Out), !.
 not_done_yet([_|In], Out) :- not_done_yet(In, Out), !.
 
+
+% is_sudoku_finished(+Sudoku, +Fields)
+is_sudoku_finished(_, []).
+is_sudoku_finished(Sudoku, [H|Tail]) :- correct(Sudoku, H, H), is_sudoku_finished(Sudoku, Tail), !.
+
 % adhoc(+Sudoku, -SolvedSudoku)
-adhoc(sudoku(M, N, Fields), sudoku(M, N, Fields)) :- 
-	not_done_yet(Fields, ND),
-	ND = [].
-  
+adhoc(sudoku(M, N, Fields), sudoku(M, N, Fields)) :-
+	not_done_yet(Fields, NotDone),
+	NotDone = [].
 adhoc(sudoku(M, N, Fields), Solved) :-
-	not_done_yet(Fields, NotDone), 
+	not_done_yet(Fields, NotDone),
 	maplist(count_correct(sudoku(M, N, Fields)), NotDone, Rated),
-	member(Rate-Field, Rated),
+	[Rate-Field|_] = Rated,
 	correct(sudoku(M, N, Fields), Field, CorrectField),
 	set_value(sudoku(M, N, Fields), Field, CorrectField, NewSudoku),
 	adhoc(NewSudoku, Solved).
-  
