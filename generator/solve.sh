56cedde6540e9fccee696222f3e6cc76bd8c849d
root <root@red.cz>
2013-03-24 11:58:33 +0100

    added adhoc solving

--- generator/solve.sh
+++ generator/solve.sh
@@ -5,7 +5,12 @@ do
 	echo "Using method 'neighbours'"
 	./../main "$sudoku" "neighbours"
 	echo
+
 	echo "Using method 'relatives'"
 	./../main "$sudoku" "relatives"
+	echo
+
+	echo "Using method 'adhoc'"
+	./../main "$sudoku" "adhoc"
 	echo
 done
